06d12c1c339919223fe7bec2c27b2e6f88922a3a
Brandon Martin <brandonmartin@Brandons-MacBook-Pro.local>
2022-09-04 16:16:56 -0600

    server.js added

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+const {Pool} = require('pg')
+const pool = new Pool ({
+    user: 'brandonmartin',
+    password: '',
+    host: 'localhost',
+    port: 5432,
+    database: 'TodoListDB'
+})
+
+app.use(express.json())
+
+app.get('/todo', async (req,res) => {
+    
+})
+
+app.post('/todo', async (req,res) => {
+    
+})
+
+app.patch('/todo', async (req,res) => {
+    
+})
+
+app.delete('/todo', async (req,res) => {
+   
+})
+
+
+app.listen(port, () => {
+    console.log('Listening on port:' + port)
+})
