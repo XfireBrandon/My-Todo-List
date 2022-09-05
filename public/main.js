27d64c291bf55d29c6ca7706b7d9302e4a5d4ca7
Brandon Martin <brandonmartin@Brandons-MacBook-Pro.local>
2022-09-05 14:26:13 -0600

    Post working and delete all working to delete table

--- public/main.js
+++ public/main.js
@@ -10,16 +10,32 @@ const input = document.querySelector('#input')
 
 input.addEventListener('keypress', async (e) => {
     if (e.key === 'Enter') {
-        const data = await fetch('http://localhost:8000/todo/post', {
+        const todos = {
+            "todo": e.target.value
+        }
+       
+        const response = await fetch('http://localhost:8000/todo/post', {
             method: 'POST',
             headers:{
-                'Content-Type': 'text/plain;charset=utf-8'
+                'Content-Type': 'application/json;charset=utf-8'
             },
-            body: JSON.stringify(e.target.value)
+            body: JSON.stringify(todos)
         });
-        let json  = await data.json();
-        alert(json)
-        console.log(e.target.value)
+        // let json  = await data.json();
+        // console.log(json)
+       
     }
 })
 
+const cBtn = document.querySelector('#clearButton')
+
+cBtn.addEventListener('click', async (e) => {
+    const data = await fetch('http://localhost:8000/todo', {
+        method: "DELETE",
+        headers: {
+            'Content-type': 'text/plain'
+        }
+    });
+    // const json = await data.json()
+    // console.log(json)
+})
