ebff57afa6e64a642dce5ad3ab307f3734aa1a64
Brandon Martin <brandonmartin@Brandons-MacBook-Pro.local>
2022-09-07 17:39:46 -0600

    All routes working, time for styling

--- public/main.js
+++ public/main.js
@@ -1,67 +1,28 @@
 const btn = document.querySelector('#mainBtn')
 const gBtn = document.querySelector('#showAll')
 const container = document.querySelector('#container')
-let btn3 = document.createElement('button')
+
+
 
 
 const input = document.querySelector('#input')
 
-function deleteTodo() {
-    const hideQuote = document.getElementsByClassName('todos')
-    $(hideQuote).empty()
+function hideTodos() {
+    const hideTodo = document.getElementsByClassName('todos')
+    $(hideTodo).empty()
 }
 
-function makeDiv(obj) {
-    const tododata = obj[0]
-    const div = document.createElement('h1')
-    div.setAttribute('id',tododata.id)
-    div.textContent = tododata.todo
+// function makeContainer() {
+// const editContainer = document.createElement('editContainer')
+// editContainer.className = 'edit'
 
-    let btn3 = document.createElement('button');
-    btn3.innerHTML = 'deleteTodo';
-    
-    const input2 = document.createElement('input')
-    input2.addEventListener('keypress', async (e) => {
-        if (e.key === 'Enter') {
-            const todos = {
-                "todo": e.target.value
-            }
-            const id = tododata.id
-       
-            const data = await fetch(`http://localhost:8000/todo/patch/${id}`, {
-             method: "PATCH",
-             headers: {
-                 'Content-type': 'application/json'
-             },
-             body: JSON.stringify(todos)
-         });
-         let json = await data.json();
-        console.log(json)
-        
-           
-        }
-        
-    })
+// const input2 = document.createElement('input')
+// editContainer.appendChild(input2)
 
 
-    appendDivToContainer(div)
-    appendDivToContainer(btn3)
-    appendDivToContainer(input2)
 
-    btn3.addEventListener('click', async (e) => {
-       const id = tododata.id
-       
-       const data = await fetch(`http://localhost:8000/todo/${id}`, {
-        method: "DELETE",
-        headers: {
-            'Content-type': 'text/plain'
-        }
-    });
-        div.remove()
-        btn3.remove()
-        input2.remove()
-    })
-}
+// container.appendChild(editContainer)
+// }
 
 function appendDivToContainer(htmlNode) {
     container.appendChild(htmlNode)
@@ -80,8 +41,8 @@ input.addEventListener('keypress', async (e) => {
             body: JSON.stringify(todos)
         });
         let data = await response.json()
-        makeDiv(data)
-       
+        // getAll()
+       window.location.reload()
         
        
     }
@@ -90,22 +51,35 @@ input.addEventListener('keypress', async (e) => {
 
 const appendTocontainer = (data) => {
     const div = document.createElement('h1')
+    div.className = 'todos'
     div.setAttribute('id',data.id)
     div.textContent = data.todo
     container.appendChild(div)
-    console.log(data.id)
+   
 }
 
-
-// gBtn.addEventListener('click', async (e) => {
-//     const data = await fetch('http://localhost:8000/todo')
-//     const json = await data.json()
-   
-//     Object.keys(json).forEach((key) => {
-//         appendTocontainer((json[key]))
-//     })
+// const deleteButton = () => {
+//     const btn3 = document.createElement('button')
+//     btn3.innerHTML = 'deleteTodo';
     
-// })
+//     btn3.addEventListener('click', async (e) => {
+//         const id = e.target.value
+//         const data = await fetch(`http://localhost:8000/todo/${id}`, {
+//          method: "DELETE",
+//          headers: {
+//              'Content-type': 'text/plain'
+//          }
+//      });
+//         const json = await data.json()
+//       console.log(json)
+//         //  div.remove()
+//         //  btn3.remove()
+        
+//      })
+//      appendDivToContainer(btn3)
+// }
+
+
 
 
  const getAll =  async () => {
@@ -114,8 +88,58 @@ const appendTocontainer = (data) => {
    
     Object.keys(json).forEach((key) => {
         appendTocontainer((json[key]))
+
+        const btn3 = document.createElement('button')
+        btn3.innerHTML = 'Delete';
+        
+        btn3.addEventListener('click', async (e) => {
+            
+            const id = json[key].id
+            const data = await fetch(`http://localhost:8000/todo/${id}`, {
+             method: "DELETE",
+             headers: {
+                 'Content-type': 'text/plain'
+             }
+         });
+         window.location.reload()
+         })
+
+         const editBtn = document.createElement('button')
+         editBtn.innerHTML = 'Submit'
+
+         editBtn.addEventListener('click', () => {
+             document.location.reload()
+         })
+
+         const input2 = document.createElement('input')
+         input2.addEventListener('keypress', async (e) => {
+             if (e.key === 'Enter') {
+                
+                 const todos = {
+                     "todo": e.target.value
+                 }
+                 const id = json[key].id
+            
+                 const data = await fetch(`http://localhost:8000/todo/patch/${id}`, {
+                  method: "PATCH",
+                  headers: {
+                      'Content-type': 'application/json'
+                  },
+                  body: JSON.stringify(todos)
+                  
+              });
+            
+            let patchData = await data.json();
+             }  
+         })
+         appendDivToContainer(btn3)
+         appendDivToContainer(input2)
+         appendDivToContainer(editBtn)
+        
     })
     
 }
 
 getAll();
+
+
